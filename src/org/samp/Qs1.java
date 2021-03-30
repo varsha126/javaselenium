c6a96e144367d060fe349f1465a93db95b57cb9a
varsha126 <varshasrini12@gmail.com>
2021-03-30 06:55:21 +0530

    code up

--- src/org/samp/Qs1.java
+++ src/org/samp/Qs1.java
@@ -1,12 +1,26 @@
 package org.samp;
 
+import java.util.Scanner;
+
 public class Qs1 {
 	public static void main(String[] args) {
 		
 		
 		System.out.println("varsha");
-		
+		System.out.println("Cse");
+		System.out.println("Greenss");
+		System.out.println("Enter any Number :");
+		Scanner s=new Scanner(System.in);
+			int n=s.nextInt();
+			int fact=1;
+			for(int i=1;i<=n;i++)
+			{
+				fact= fact*i;
+			}
+			System.out.println("Factorial of number :"+fact);
+		}
+
 		
 	}
 
-}
+
